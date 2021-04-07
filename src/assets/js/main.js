676df4c21435bb95b5ca0db7a7081c919a66385f
7noptic <7noptic@gmail.com>
2021-04-07 10:29:25 +0300

    make

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,49 +9,152 @@ Swiper.use([Navigation, Pagination, Autoplay]);
 window.addEventListener('DOMContentLoaded', () => {
     /* HAMBURGER MENU IN HEADER*/
     let header = document.querySelector('.header'),
-        hamburgerBtn = header.querySelector('.js-burger'),
-        hamburgerMenu = header.querySelector('.hamburger-menu');
+    main = document.querySelector('main'),
+    footer = document.querySelector('.footer'),
+    searchForm = header.querySelector('.form-wrapper-header'),
+    hamburgerBtn = header.querySelector('.js-burger'),
+    hamburgerMenu = header.querySelector('.hamburger-menu');
 
 
-    header.addEventListener('click', (e) => {
-        if (e.target && e.target.classList.contains('js-burger')) {
-            hamburgerMenu.classList.toggle('active');
-            hamburgerBtn.classList.toggle('active');
-        }
-    });
-    /* ЗАГРУЗКА ДАННЫХ ИЗ LocalStorage */
-    /* пример
-    if(localStorage.getItem('city') != null){
-        regionBtn.innerHTML = localStorage.getItem('city');
-    } else{
-        regionBtn.innerHTML = 'Москва';
+header.addEventListener('click', (e) => {
+    if (e.target && e.target.classList.contains('js-burger')) {
+        hamburgerMenu.classList.toggle('active');
+        hamburgerBtn.classList.toggle('active');
     }
-    */
-
-    /* MODAL */
-    let modalBlock = document.querySelector('.js-sidebars'),
-        allModal = document.querySelectorAll('.js-sidebars > section');
-
-
-    document.addEventListener('click', e => {
-        let target = e.target;
-        /* ЗАКРЫТИЕ ПО КЛИКУ НА САЙДБАР */
-        if (target && target.classList.contains('sidebar-bg')) {
-            e.preventDefault();
-            modalBlock.classList.toggle('sidebar-bg');
-            for (let i = 0; i < allModal.length; i++) {
-                if (allModal[i].classList.toggle('active')) {
-                    allModal[i].classList.remove('active');
-                }
-            }
-        }
-    });
-
-    function toggleModal(e, modal) {
-        e.preventDefault();
-        modalBlock.classList.toggle('sidebar-bg');
-        modal.classList.toggle('active');
+    if (e.target && e.target.classList.contains('js-search')) {
+        searchForm.classList.toggle('active');
     }
+});
+      /* MODAL */
+      let modalBlock = document.querySelector('.js-sidebars'),
+      allModal = document.querySelectorAll('.js-sidebars > section'),
+      modalCall = document.querySelector('.modal-call'),
+      modalQuest = document.querySelector('.modal-quest'),
+      modalRegion = document.querySelector('.modal-region'),
+      modalOneClick = document.querySelector('.modal-one-click'),
+      modalAssortment = document.querySelector('.modal-assortment'),
+      modalJob = document.querySelector('.modal-job'),
+      modalPersonal = document.querySelector('.modal-personal'),
+      modalResetPass = document.querySelector('.modal-reset-pass'),
+      modalSubscribe = document.querySelector('.modal-subscribe'),
+      modalReviews = document.querySelector('.modal-reviews'),
+      regionSelect = document.querySelectorAll('.modal-region__link'),
+      regionBtn = document.querySelector('.js-region-city'),
+      burgerMenu = document.querySelector('.adaptive-menu');
+
+  if (localStorage.getItem('city') != null) {
+      console.log(localStorage.getItem('city'));
+      console.log(typeof (localStorage.getItem('city')));
+      regionBtn.innerHTML = localStorage.getItem('city');
+  } else {
+      regionBtn.innerHTML = 'Москва';
+  }
+  document.addEventListener('click', e => {
+
+      let target = e.target;
+
+      if (target && (target.classList.contains('js-call') || target.classList.contains('modal-call__exit'))) {
+          openCloseModal(e, modalCall);
+      }
+      if (target && (target.classList.contains('js-reviews') || target.classList.contains('modal-reviews__exit'))) {
+          openCloseModal(e, modalReviews);
+      }
+      if (target && (target.classList.contains('js-modal-header') || target.classList.contains('adaptive-menu__exit'))) {
+          openCloseModal(e, burgerMenu);
+      }
+      if (target && (target.classList.contains('js-one-click') || target.classList.contains('modal-one-click__exit'))) {
+          openCloseModal(e, modalOneClick);
+      }
+      if (target && (target.classList.contains('js-job') || target.classList.contains('modal-job__exit'))) {
+          openCloseModal(e, modalJob);
+      }
+      if (target && (target.classList.contains('js-subscribe') || target.classList.contains('modal-subscribe__exit'))) {
+          openCloseModal(e, modalSubscribe);
+      }
+      if (target && (target.classList.contains('js-assortment') || target.classList.contains('modal-assortment__exit'))) {
+          openCloseModal(e, modalAssortment);
+      }
+      if (target && (target.classList.contains('js-region') || target.classList.contains('modal-region__exit'))) {
+          openCloseModal(e, modalRegion);
+      }
+      if (target && (target.classList.contains('js-quest') || target.classList.contains('modal-quest__exit'))) {
+          openCloseModal(e, modalQuest);
+      }
+      if (target && (target.classList.contains('js-personal-data') || target.classList.contains('modal-personal__exit'))) {
+          openCloseModal(e, modalPersonal);
+      }
+      if (target && (target.classList.contains('js-reset-pass') || target.classList.contains('modal-reset-pass__exit'))) {
+          openCloseModal(e, modalResetPass);
+      }
+      if (target.classList.contains('modal-region__link')) {
+          for (let i = 0; i < regionSelect.length; i++) {
+              if (regionSelect[i] == target) {
+                  let citiValue = regionSelect[i].innerHTML;
+                  localStorage.setItem('city', citiValue)
+                  let testValue = localStorage.getItem('city')
+                  if (testValue == 'undifiend') {
+                      regionBtn.innerHTML = 'Москва';
+                  } else {
+                      regionBtn.innerHTML = testValue.innerHTML;
+                      regionBtn.innerHTML = testValue;
+                  }
+
+              }
+          }
+          /*
+          regionSelect.forEach(item => {
+              if (item == target) {
+                  let citiValue = item.innerHTML;
+                  localStorage.setItem('city', citiValue)
+                  let testValue = localStorage.getItem('city')
+                  regionBtn.innerHTML = testValue.innerHTML;
+                  regionBtn.innerHTML = testValue;
+              }
+          });
+          */
+          openCloseModal(e, modalRegion);
+      } else if (target.classList.contains('js-region-close')) {
+          let city = document.querySelector('.js-select-city');
+          localStorage.setItem('city', city.innerHTML);
+          let testValue = localStorage.getItem('city');
+          regionBtn.innerHTML = testValue;
+          openCloseModal(e, modalRegion);
+      }
+
+
+      /* ЗАКРЫТИЕ ПО КЛИКУ НА САЙДБАР */
+      if (target && target.classList.contains('sidebar-bg')) {
+          e.preventDefault();
+          //html.classList.toggle('lock');
+          modalBlock.classList.toggle('sidebar-bg');
+          for (let i = 0; i < allModal.length; i++) {
+              if (allModal[i].classList.toggle('active')) {
+                  allModal[i].classList.remove('active');
+              }
+          }
+          header.classList.toggle('blur');
+          main.classList.toggle('blur');
+          footer.classList.toggle('blur');
+          /*
+          allModal.forEach(item => {
+              if (item.classList.toggle('active')) {
+                  item.classList.remove('active');
+              }
+          });*/
+      }
+
+  });
+
+  function openCloseModal(e, modal) {
+      e.preventDefault();
+      //html.classList.toggle('lock');
+      //body.classList.toggle('lock');
+      modalBlock.classList.toggle('sidebar-bg');
+      modal.classList.toggle('active');
+      header.classList.toggle('blur');
+      main.classList.toggle('blur');
+      footer.classList.toggle('blur');
+  }
 
     let headerIconsParent = document.querySelector('.header-category'),
         headerIconsLink = document.querySelectorAll('.js-header-icon-link'),
@@ -64,7 +167,10 @@ window.addEventListener('DOMContentLoaded', () => {
         headerKiotTab = document.querySelectorAll('.js-header-kiot-tabs'),
         storeParent = document.querySelector('.store'),
         storeLink = document.querySelectorAll('.js-store-link'),
-        storeTab = document.querySelectorAll('.js-store-tab');
+        storeTab = document.querySelectorAll('.js-store-tab'),
+        newsParent = document.querySelector('.news-block'),
+        newsLink = document.querySelectorAll('.js-news-link'),
+        newsTab = document.querySelectorAll('.js-news-tab');
 
 
     if (headerIconsParent && headerShelfLink.length > 0) {
@@ -75,6 +181,9 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     if (storeParent && storeLink.length > 0) {
         toggleTabs(storeLink, storeTab, storeParent, 'js-store-link', true, '.js-store-sublink', '.js-store-subtab');
+    }
+    if (newsParent && newsLink.length > 0) {
+        toggleTabs(newsLink, newsTab, newsParent, 'js-news-link');
     }
 
     /* TABS */
@@ -140,12 +249,23 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+
     /* SHOW HIDE CONTENT */
 
+    let popularParent = document.querySelector('.popular'),
+        popularLink = document.querySelectorAll('.popular__head'),
+        popularContent = document.querySelectorAll('.popular__content');
+
+    if(popularParent && popularLink.length > 0 && popularContent.length > 0){
+        toggleContent(popularLink, popularContent, 'popular__head');
+    }
+
     function toggleContent(link, content, linkClass) {
+
         document.addEventListener('click', (e) => {
             if (e.target && e.target.classList.contains(linkClass)) {
                 e.preventDefault();
+
                 for (let i = 0; i < link.length; i++) {
                     if (e.target == link[i]) {
                         link[i].classList.toggle('active');
@@ -172,7 +292,45 @@ window.addEventListener('DOMContentLoaded', () => {
             clickable: true,
         },
     });
+    let SwiperNews = new Swiper('.swiper-news', {
+        slidesPerView: 1,
+        spaceBetween: 50,
+        observeParents: true,
+        observer: true,
+        allowSlidePrev: true,
+        allowSlideNext: true,
+        
+        pagination: {
+            el: '.swiper-pagination-news',
+            clickable: true,
+        },
+    });
+    let sliderTags = new Swiper('.swiper-container-tags', {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        observeParents: true,
+        observer: true,
+        allowSlidePrev: true,
+        allowSlideNext: true,
 
+        navigation: {
+            nextEl: '.tags__next',
+            prevEl: '.tags__prev'
+        },
+        breakpoints: {
+            0:{
+                spaceBetween: 10,
+            },
+            767:{
+                spaceBetween: 15,
+            },
+            991:{
+                spaceBetween: 20,
+            }
+        }
+
+
+    });
 
     /* RATING */
     let ratingParent = document.querySelector('.js-rating'),
